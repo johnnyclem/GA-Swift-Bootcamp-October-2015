fd6f8d89b8d3c02ae32271582636162fa6c5a843
John Clem <johnnyclem@gmail.com>
2015-10-10 13:38:28 -0700

    initial commit

---
+++ RetroCamera/RetroCamera/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  RetroCamera
+//
+//  Created by John Clem on 10/10/15.
+//  Copyright © 2015 learnSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+//        self.view.backgroundColor = UIColor.blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        print("button was pressed")
+        self.mainLabel.text = "Button Tapped"
+    }
+}
+
